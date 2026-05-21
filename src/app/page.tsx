d7610924d8da30bc8d1252d3609e76f5a849e29a
Juwan Petty <jchpetty@gmail.com>
2026-05-20 21:37:25 -0400

    Implement PreviewFrame component and integrate it into multiple pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { PreviewFrame } from "@/components/preview-frame";
+
 export default function HomeIndex() {
   return (
     <div className="grid place-items-center h-svh w-full bg-white dark:bg-black">
-      Home Page
+      <PreviewFrame
+        defaultTheme="dark"
+        showThemeToggle={false}
+        src="/dark-mode-toast"
+      />
     </div>
   );
 }
